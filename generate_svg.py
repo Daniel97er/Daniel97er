a00363bc9fd8f672dc06c2feba57be605fd26bf2
Daniel <108468246+Daniel97er@users.noreply.github.com>
2024-10-24 16:06:43 +0200

    Update generate_svg.py

--- generate_svg.py
+++ generate_svg.py
@@ -38,9 +38,6 @@ query = """
 response = requests.post(url, headers=headers, json={'query': query})
 data = response.json()
 
-# Überprüfe die gesamte Antwort
-print(data)
-
 # Sammeln der Sprachdaten
 languages = {}
 repos = data.get('data', {}).get('viewer', {}).get('repositories', {}).get('edges', [])
